25c84cae444fe69b2028049da9d5c88df5f34a38
aqib <aqib.hayat@yahoo.com>
2017-09-14 05:29:52 +0500

    all files added

---
+++ src/main/java/com/nisum/SpringBootStarter.java
@@ -0,0 +1,15 @@
+package com.nisum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarter {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootStarter.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/nisum/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.nisum.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHi() {
+
+		return "Hello Spring Boot Application";
+
+	}
+
+}
